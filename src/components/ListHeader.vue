16bb2ae5b5af5866584454d0c03aa5f9772a1e4d
himantyu-yuma <lipton.happy.milktea@gmail.com>
2022-06-18 21:37:19 +0900

    #9 一覧表示のヘッダーコンポーネントを作成

---
+++ src/components/ListHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="py-3" :style="styles">
+    <h1 class="text-center">
+      <slot />
+    </h1>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'ListHeaderComponent',
+  props: {
+    color: {
+      type: String,
+      default: 'blue'
+    }
+  },
+  computed: {
+    styles () {
+      return {
+        '--color': this.color
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  background-color: var(--color);
+}
+</style>
