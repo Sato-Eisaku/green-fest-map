dcc799da1be94501300405e532e54d66073c6539
himantyu-yuma <lipton.happy.milktea@gmail.com>
2022-06-18 23:07:47 +0900

    #9 詳細画面の本文用コンポーネントを作成

---
+++ src/components/DetailBody.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="mx-10" v-text="detailText" />
+</template>
+
+<script>
+export default {
+  name: 'DetailBodyComponent',
+  props: {
+    detailText: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  white-space: pre-wrap;
+}
+</style>
